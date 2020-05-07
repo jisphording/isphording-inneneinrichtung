fda753bb1ca3b2ed32ea09e75e2d2cee4e13a21e
Johannes Isphording <mail@johannesisphording.de>
2020-05-07 17:50:28 +0200

    [template][css] Added a read more insert to page

---
+++ site/snippets/insert-readmore.php
@@ -0,0 +1,11 @@
+<section class="insert-readmore">
+<!-- This Insert can be used as a hook/link to another page which contains more information on the topic and/or to break up the page flow a little bit -->
+    
+    <div class="column01">
+        <p>Lesen Sie mehr <em>in diesem Artikel:</em>
+    </div>
+    <div class="column02">
+        <?= $page -> InsertReadmore() -> kirbytext() ?>
+    </div>
+
+</section>
