8fdeb903e45b5239e7a1f924afae1c6eba23c9e8
Johannes Isphording <mail@johannesisphording.de>
2020-05-07 19:35:17 +0200

    [template] added a canvas as a noise background

--- site/snippets/insert-readmore.php
+++ site/snippets/insert-readmore.php
@@ -1,11 +1,13 @@
 <section class="insert-readmore">
 <!-- This Insert can be used as a hook/link to another page which contains more information on the topic and/or to break up the page flow a little bit -->
-    
-    <div class="column01">
-        <p>Lesen Sie mehr <em>in diesem Artikel:</em>
-    </div>
-    <div class="column02">
-        <?= $page -> InsertReadmore() -> kirbytext() ?>
-    </div>
-
+    <div class="insert-readmore__content">
+        <div class="column01">
+            <p>Lesen Sie mehr <em>in diesem Artikel:</em>
+        </div>
+        <div class="column02"> 
+            <?= $page -> InsertReadmore() -> kirbytext() ?>
+        </div>
+   </div>
+   
+   <canvas id="insert-readmore__grain" class="insert-readmore__grain"><!-- only background animation via css --></canvas>
 </section>
