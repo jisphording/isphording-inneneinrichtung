a6ea5de03a6977f7739b455cdbe5c855a22c66c4
Johannes Isphording <mail@johannesisphording.de>
2020-05-18 23:45:08 +0200

    [template][html] reworked footer structure
    to better prepare the footer for styling with css grid

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -9,8 +9,8 @@
 
     <section class="footer-navigation black-bg">
     <?php if ($contact = page('contact')): ?>
-      <ul>
-        <ul>
+      <ul class="footer-navigation__menu__main">
+        <ul class="footer-navigation__menu__sub">
           <li>Navigation</li>
           <nav class="menu">
             <?php foreach ($site->children()->listed() as $item): ?>
@@ -18,7 +18,7 @@
             <?php endforeach ?>
           </nav>
         </ul>
-        <ul>
+        <ul class="footer-navigation__menu__sub">
           <li>Kontakt</li>
           <li>Mainstraße 125</li>
           <li>41469 Neuss</li>
@@ -27,11 +27,11 @@
           <li>mail@isphording-inneneinrichtung.de</li>
           <li>+49 2137 92 76 72</li>
         </ul>
-        <ul>
+        <ul class="footer-navigation__menu__sub">
           <li>Legal</li>
           <li><a href="<?= $site->url() ?>/imprint">Impressum</a></li>
         </ul>
-        <ul>
+        <ul class="footer-navigation__menu__sub">
           <li>Folgen Sie Uns</li>
           <li><?php foreach ($contact->social()->toStructure() as $social): ?>
             <a href="<?= $social->url() ?>"><?= $social->platform() ?></a>
@@ -42,15 +42,17 @@
     </section>
 
     <section class="footer-bottom silver-bg">
-      <p>Wir sind spezialisiert auf die Fertigung <strong>individueller Einzelstücke</strong> und die Innenausstattung mit hochwertigen und <strong>exklusiven Stoffen.</strong></p>
-
-      <img class="emblem" src="<?= $kirby->url('assets') ?>/img/emblem.svg">
-
-      <div class="copyright">
-        <p>Crafted <a class="date" href="<?= url() ?>"> <?= date('Y') ?></a> by <a href="http://studioisphording.com/">Studio Isphording</a></p>
-        <span>Alle Rechte Vorbehalten</span>
-        <span class="trademark">&copy;</span>
-      </div>
+      <ul>
+        <li class="specialized">Wir sind spezialisiert auf die Fertigung <strong>individueller Einzelstücke</strong> und die Innenausstattung mit hochwertigen und <strong>exklusiven Stoffen.</strong>
+        </li>
+        <li class="emblem">
+          <img src="<?= $kirby->url('assets') ?>/img/emblem.svg">
+        </li>
+        <li class="copyright">
+            <p>Crafted <a class="date" href="<?= url() ?>"> <?= date('Y') ?></a> by <a href="http://studioisphording.com/">Studio Isphording</a></p>
+            <span>Alle Rechte Vorbehalten</span>
+            <span class="trademark">&copy;</span>
+        </li>
     </section>
   </footer>
 
