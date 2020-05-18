4396c16fd854a10ee106285e5c420a190d77ef51
Johannes Isphording <mail@johannesisphording.de>
2020-05-18 18:35:32 +0200

    [template][php] new insert added

---
+++ site/snippets/insert-other-projects.php
@@ -0,0 +1,31 @@
+<section class="insert-other-projects silver-bg-light">
+    <div class="insert-other-projects__headline">
+        <span>Andere Projekte</span>
+    </div>
+
+    <section class="projects">
+    <ul>
+      <?php foreach(page('projects')->children()->published()->limit(4) as $project): ?>
+      <li>
+        <!-- Fetching only the first image, will be exchanged for a cover image later. -->
+        <?php if($image = $project->image()): ?>
+            <figure class="grid__item grid__item--image">
+                <img class="grid__image" src="<?= $image->url() ?>" alt="<?= $project->content()->title() ?>">
+            </figure>
+        <?php endif ?>
+
+        <div class="grid__container--text">
+          <div class="grid__item grid__item--headline">
+            <h2 class="project__title"><?= $project->title() ?></h2>
+            <h2 class="project__subtitle"><?= $project->subtitle() ?></h2>
+          </div>
+          <div class="grid__item grid__item--bodycopy">
+            <p class="project__synopsis"><?= $project->synopsis() ?></p>
+          </div>
+
+      </li>
+      <?php endforeach ?>
+    </ul>
+  </section>
+
+</section>
