c7c5d3341eef92a6bc47f4ff01dba4322f8ca0d5
Johannes Isphording <mail@johannesisphording.de>
2020-05-23 22:54:58 +0200

    [cms] Fixed a typo

--- site/templates/services.php
+++ site/templates/services.php
@@ -41,7 +41,7 @@
 <?php snippet('introMood') ?>
 
 <section class="mood__claim">
-	<?= $page -> Claim() -> kirbytext() ?>
+	<?= $page -> Claim() -> kirby() ?>
 </section>
 
 	<section class="project__text--longform">
