8e5ebeb97eed52abc0aa493aa6b1db97cc7b1701
Johannes Isphording <mail@johannesisphording.de>
2020-05-23 16:00:33 +0200

    [typo] removed space

--- site/snippets/intro.php
+++ site/snippets/intro.php
@@ -7,7 +7,7 @@
         $introtxt_ar = explode(', ', $introtxt); 
         ?>
     
-        <h1 class="color-inverted"><?php print $introtxt_ar[0] ;?></h1>
+        <h1 class="color-inverted"><?php print $introtxt_ar[0]; ?></h1>
         <h1><?php print $introtxt_ar[1]; ?></h1>
         <h1><?php print $introtxt_ar[2]; ?></h1>
     </div>
