1ff94827c99e9fb135691ae545884cfa54efb213
Johannes Isphording <mail@johannesisphording.de>
2020-08-19 14:34:00 +0200

    [php][template][func] Adding image counter to array function

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -15,7 +15,8 @@
 
 		// make use of kirbys filter function to load web optimized images for all images of the site
 		$images = $currentpage->images()->filterBy('extension', 'webp');
-
+		$figcount = 0; // used inside function for numbering images
+		
 		foreach($images as $file ):
 			$images_ar[] = $file;
 		endforeach;
@@ -24,6 +25,9 @@
 		// This function takes a kirby files object and can be called in this template to place the first image from this object along with it's meta information and then remove it from the array to avoid duplication
 		// -> the '&' infront of the variable is used to pass a reference to the variable instead of a copy
 		function unshift_image(&$el) {
+			// #TODO - Counting the number of images automatically. Using a global variable here is not clean and should be changed.
+			global $figcount; 
+
 			// First, we check if the array actually contains any images
 			if (empty($el)) {
 				// Do nothing, because array is empty and/or all images are already placed
@@ -34,11 +38,14 @@
 
 					<figcaption>
 						<span class="title"><p><?= $el[0]->content()->title() ?></p></span>
-						<span class="caption"><p><?= $el[0]->content()->caption() ?></p></span>
+						<span class="caption"><p><?= $el[0]->content()->caption(); echo ($figcount); ?></p></span>
 					</figcaption>
 				</figure>
 				<?php
 				array_shift($el);
+
+				$figcount += 1;
+				return $figcount;
 			}	
 		}
 	?>
