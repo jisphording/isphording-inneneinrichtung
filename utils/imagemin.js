c1d118dd9eb42046bf9079d8aa4ae1076ccbeb0b
Johannes Isphording <mail@johannesisphording.de>
2020-11-29 18:03:09 +0100

    [setup] changed npm scripts for dist build delivery

--- utils/imagemin.js
+++ utils/imagemin.js
@@ -1,7 +1,24 @@
 // keep folder structure as input use imagemin-webp
 const imagemin = require('imagemin-keep-folder');
 const imageminWebp = require("imagemin-webp");
- 
+const imageminSvgo = require('imagemin-svgo');
+
+// Run imagemin on svg files found in assets
+imagemin(['assets/img/*.svg'], 'dist/img', {
+  use: [
+      imageminSvgo({
+          plugins: [
+              { optimizationLevel: 3 },
+              { progessive: true },
+              { interlaced: true },
+              { removeViewBox: false },
+              { removeUselessStrokeAndFill: false },
+              { cleanupIDs: false },
+          ]
+      })
+  ]
+});
+
 // Run imagemin on content images that are kept in same folder
 imagemin(['content/**/*.{jpg,png}'], {
   use: [
